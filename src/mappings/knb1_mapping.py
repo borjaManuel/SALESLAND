297df46666d6d966fe70edcfc948085f679acef0
Borja Martinez <bmmartinez@teknei.com>
2026-07-27 13:58:22 +0200

    feat: add KNB1 data mapper

---
+++ src/mappings/knb1_mapping.py
@@ -0,0 +1,11 @@
+KNB1_MAPPING = {
+    "Deudor": "kunnr",
+    "Sociedad": "bukrs",
+    "Cuenta asociada": "akont",
+    "Vías de pago": "zwels",
+    "Condiciones de pago": "zterm",
+    "Creado por": "ernam",
+    "Bloqueo contab. para sociedad": "sperr",
+    "Petición de borrado p.sociedad": "loevm",
+    "Creado el": "erdat",
+}
