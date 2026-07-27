8179a4dcde09362fe0110ca641612a833f4328ee
Borja Martinez <bmmartinez@teknei.com>
2026-07-27 15:33:52 +0200

    feat: add SKA1 data mapper

---
+++ src/mappings/ska1_mapping.py
@@ -0,0 +1,14 @@
+SKA1_MAPPING = {
+    "Plan de cuentas": "ktopl",
+    "Cuenta de mayor": "saknr",
+    "Cuenta de balance": "xbilk",
+    "Creado el": "erdat",
+    "Creado por": "ernam",
+    "Tp.cta.beneficios": "gvtyp",
+    "Grupo de cuentas": "ktoks",
+    "Marcar para borrado": "xloev",
+    "Bloqueada para creación": "xspea",
+    "Bloqueada p.contabil.": "xspeb",
+    "Bloqueada para planif.": "xspec",
+    "Texto explicativo": "txt50",
+}
