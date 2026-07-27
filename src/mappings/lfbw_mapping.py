1fba52cd682202e55e776d9b7c5a802b3ebe6b09
Borja Martinez <bmmartinez@teknei.com>
2026-07-27 14:52:17 +0200

    feat: add LFBW data mapper

---
+++ src/mappings/lfbw_mapping.py
@@ -0,0 +1,14 @@
+LFBW_MAPPING = {
+    "Acreedor": "lifnr",
+    "Sociedad": "bukrs",
+    "Tipo de retenciones": "witht",
+    "Sujeto a retención": "wt_withcd",
+    "Categoría retención": "wt_subjct",
+    "Nº identificación fiscal": "wt_exrt",
+    "Indicador de retención": "wt_exdf",
+    "Número certificado exención": "wt_exdt",
+    "Tipo de exención": "wt_wtexm",
+    "Fe.inicio exención": "wt_exrs",
+    "Fecha fin exención": "wt_wtstcd",
+    "Motivo de exención": "wt_withtr",
+}
