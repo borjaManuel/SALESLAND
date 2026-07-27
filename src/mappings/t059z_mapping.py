972e37ac586188112f4dfe27df2d13c5bbf0da1f
Borja Martinez <bmmartinez@teknei.com>
2026-07-27 15:57:08 +0200

    feat: add T059Z data mapper

---
+++ src/mappings/t059z_mapping.py
@@ -0,0 +1,8 @@
+T059Z_MAPPING = {
+    "Clave de país": "land1",
+    "Tipo de retenciones": "witht",
+    "Indicador de retención": "wt_withcd",
+    "Denominación": "text40",
+    "Parte sujeta a retención": "wt_qsbase",
+    "Tipo de retención": "wt_wtreci",
+}
