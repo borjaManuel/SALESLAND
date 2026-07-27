c298e122c31c89514d34c7a3781a7cd292d8e87d
Borja Martinez <bmmartinez@teknei.com>
2026-07-27 13:39:31 +0200

    feat: add ANLA data mapper

---
+++ src/mappings/anla_mapping.py
@@ -0,0 +1,8 @@
+ANLA_MAPPING = {
+    "Sociedad": "bukrs",
+    "Activo fijo": "anln1",
+    "Subnúmero": "anln2",
+    "Clase de activo fijo": "anlkl",
+    "Denominación del activo fijo": "txt50",
+    "Fecha de creación": "erdat",
+}
