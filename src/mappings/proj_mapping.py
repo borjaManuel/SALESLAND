74433099560867f4ac50ba863e96cf3bfc3c0337
Borja Martinez <bmmartinez@teknei.com>
2026-07-27 15:22:34 +0200

    feat: add PROJ data mapper

---
+++ src/mappings/proj_mapping.py
@@ -0,0 +1,9 @@
+PROJ_MAPPING = {
+    "Definición del proyecto": "pspid",
+    "Denominación": "post1",
+    "Sociedad": "vbukr",
+    "Moneda del proyecto": "waers",
+    "Fecha inicio": "plfaz",
+    "Fecha fin": "plsez",
+    "Perfil de proyecto": "profl",
+}
