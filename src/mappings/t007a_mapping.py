0db50504a877d3302ca9b7adcfa14753b154eae1
Borja Martinez <bmmartinez@teknei.com>
2026-07-27 15:41:56 +0200

    feat: add T007A data mapper

---
+++ src/mappings/t007a_mapping.py
@@ -0,0 +1,7 @@
+T007A_MAPPING = {
+    "Esquema": "kalsm",
+    "Indicador impuestos": "mwskz",
+    "Clase de impuesto": "mwskt",
+    "Identif UE/Identif.": "egbld",
+    "Significado": "text1",
+}
