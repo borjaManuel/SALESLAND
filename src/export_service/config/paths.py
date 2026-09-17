4e04f31877c9ed08d3d633c7f256b8e505cb849d
Borja Martinez <bmmartinez@teknei.com>
2026-09-17 15:55:27 +0200

    chore(paths): add export service path configuration

---
+++ src/export_service/config/paths.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+# Root directory of the project
+ROOT_DIR = Path(__file__).resolve().parents[3]
+
+# Src directory of the project
+SRC_DIR = ROOT_DIR / "src"
+
+# Logs directory of the project
+LOG_DIR = ROOT_DIR / "logs"
